2c2aaee6f9b20d70ded5434ffb4c0407ec235aed
Sidney3 <sidneyarthurlevine1@hotmail.com>
2022-12-10 20:51:43 -0500

    feat: full draft of grid is done. Now needs to be tested properly

--- src/Grid.ts
+++ src/Grid.ts
@@ -1,7 +1,9 @@
 import {VisualObject, Coords} from './VisualObject.js'
+import {Line} from './Line.js'
+import {Rectangle} from './Rectangle.js'
 
 interface gridProps{
-    coords: Coords, //note: coords refers to the top left portion of the grid
+    grid_location: Coords, //note: coords refers to the top left portion of the grid
     cell_size:{
         x_size:number,
         y_size:number
@@ -10,10 +12,6 @@ interface gridProps{
         height:number,
         width:number
     },
-    // grid_location:{ //note: this is the upper left location
-    //     x:number,
-    //     y:number
-    // },
     // outline: boolean
 
 }
@@ -26,59 +24,172 @@ interface gridCell{
 
 
 export class Grid{
-    /*
-    As one of the most common expressions of a graph is a matrix, we offer functionality
-    for building a grid of cells, where you can add visual objects to each square in the grid,
-    and they are automatically formatted into the grid (where the center of the object is aligned
-    to the center of the grid)
+    /**
+     * 
+     * As one of the most common expressions of a graph is a matrix, we offer functionality
+     * for building a grid of cells, where you can add visual objects to each square in the grid,
+     * and they are automatically formatted into the grid (where the center of the object is aligned
+     * to the center of the grid)
+     * 
+     *  Note: grid size is fixed! You can't change the size of a grid once it's created
+     * 
+     */
 
-    Note: grid size is fixed! You can't change the size of a grid once it's created
-    */
     config: gridProps
     cells: Array<Array<gridCell>>
+    gridlines: Array<Line>
 
     constructor(config: gridProps){
+        //todo: remove ? once we have pane functionality
         this.config = config
         this.initialize_cells()
     }
 
+
     initialize_cells(){
+    /**
+     * 
+     * Fill in the cells of the grid with blank objects (note: this is where we
+     * do the computations as to where the centers of objects are)
+     * 
+     */
         this.cells = []
-        for(let w = 0; w < this.config.grid_dimensions.width; w++){
+        for(let x_coord = 0; x_coord < this.config.grid_dimensions.width; x_coord++){
             this.cells.push([]);
-            for(let r = 0; r < this.config.grid_dimensions.height; r++){
+            for(let y_coord = 0; y_coord < this.config.grid_dimensions.height; y_coord++){
                 const empty_cell:gridCell = {
                     full: false,
                     center:{
-                        x:this.config.coords.x + this.config.cell_size.x_size*w + 
+                        x:this.config.grid_location.x + this.config.cell_size.x_size*x_coord + 
                         + this.config.cell_size.x_size/2,
-                        y:this.config.coords.y + this.config.cell_size.y_size*r + 
+                        y:this.config.grid_location.y + this.config.cell_size.y_size*y_coord + 
                         + this.config.cell_size.y_size/2,
                     }
                 }
-                console.log("w: " + w + ",r: " + r + "\n x center: " + empty_cell.center.x + " y center: " + empty_cell.center.y)
-                this.cells[w].push(empty_cell)
+                // console.log("w: " + w + ",r: " + r + "\n x center: " + empty_cell.center.x + " y center: " + empty_cell.center.y)
+                this.cells[x_coord].push(empty_cell)
             }
         }
     }
 
 
-    fill_cell(x_coord: number, y_coord:number, add_object:VisualObject){
-        if(!Number.isInteger(x_coord) || Number.isInteger(y_coord)){
+    fill_cell(coords: Coords, add_object:VisualObject){
+    /**
+     * Given valid coordinates of our grid, we add and center an object to a given
+     * coordinate (note: we don't support adding multiple VisualObjects to the same frame -
+     * they must be conjoined)
+     * 
+     * TODO: add feature that, when we add a new object to a cell in the grid that already has
+     * object, we make a new VisualObject that is that object conjoined with the new object
+     * 
+     * (creating a conjoined visual object shouldn't be too tough) 
+     */
+        this.check_coords(coords)
+
+        //TODO: check for inside bounding box
+
+        const target_cell: gridCell = this.cells[coords.x][coords.y]
+        target_cell.full = true
+        target_cell.contents = add_object
+        add_object.setCenter(target_cell.center) //center object
+    }
+
+    remove_cell(coords: Coords, add_object:VisualObject){
+        /**
+         * Given valid coordinates of our grid, we remove the object in a given cell
+         * 
+         * If no such object exists, we don't do anything
+         */
+            this.check_coords(coords)
+        
+            const target_cell: gridCell = this.cells[coords.x][coords.y]
+            if(target_cell.full == true){
+                target_cell.full = false
+                delete target_cell['contents']
+            }
+        }
+
+    fill_grid_lines(){
+    /**
+     * We offer the option to have our grid have line boundaries be filled in. 
+     * 
+     * Calling this method adds these lines to be rendered (calling more than once does nothing)
+     */
+        this.gridlines = []
+        //cols
+        for(let x_coord = 0; x_coord < this.config.grid_dimensions.width; x_coord++){
+            const vertLine: Line = new Line([
+                {x:this.config.grid_location.x+x_coord*this.config.cell_size.x_size,
+                    y:this.config.grid_location.y},
+                {x:this.config.grid_location.x+x_coord*this.config.cell_size.x_size,
+                    y:this.config.grid_location.y + this.config.grid_dimensions.height*this.config.cell_size.y_size}
+            ]);
+            this.gridlines.push(vertLine)
+        }
+        //rows
+        for(let y_coord = 0; y_coord < this.config.grid_dimensions.height; y_coord++){
+            const horizLine: Line = new Line([
+                {x:this.config.grid_location.x,
+                    y:this.config.grid_location.y+y_coord*this.config.cell_size.y_size},
+                {x:this.config.grid_location.x+this.config.grid_dimensions.width*this.config.cell_size.x_size,
+                    y:this.config.grid_location.y+y_coord*this.config.cell_size.y_size}
+            ]);
+            this.gridlines.push(horizLine)    
+        }
+    }
+
+    fill_solid(coords: Coords, color: string){
+        /**
+         * Given a single coordinate square of our grid, we fill that
+         * square in with a given color
+         */
+        this.check_coords(coords)
+        const target_cell: gridCell = this.cells[coords.x][coords.y]
+        target_cell.full = true
+
+        const addRectangle:Rectangle = new Rectangle({x:0,y:0},
+            this.config.cell_size.x_size, //height
+            this.config.cell_size.y_size) //width
+        
+        //TODO: set rectangle color (we don't have that functionality currently in Rectangle.ts)
+
+        this.fill_cell(coords, addRectangle)
+    }
+
+    check_coords(coords:Coords){
+        /**
+         * (This function ensures validity of inputted coords)
+         * 
+         * Given a set of coords passed into a function involving the grid coordinates, we verify that
+         * these coordinates are positive integers within the bounds of the coordinate size
+         */
+        if(!Number.isInteger(coords.x) || Number.isInteger(coords.y)){
             throw "non-integer indices given for grid coords";
         }
-        if(x_coord < 0 || y_coord < 0){
+        if(coords.x < 0 || coords.y < 0){
             throw "negative indices given for grid coords";
         }
-        if(x_coord > this.config.cell_size.x_size-1 || y_coord > this.config.cell_size.y_size-1){
+        if(coords.x > this.config.cell_size.x_size-1 || coords.y > this.config.cell_size.y_size-1){
             throw `coordinates out of bounds. Grid is of width ${this.config.cell_size.x_size} and height ${this.config.cell_size.y_size}`
         }
-        //check for inside bounding box
+    }
+
+    render(svg){
+        //render gridlines
+
+        //render each child in each cell
+        for(let x_coord = 0; x_coord < this.config.grid_dimensions.width; x_coord++){
+            for(let y_coord = 0; y_coord < this.config.grid_dimensions.height; y_coord++){
+                if(this.cells[x_coord][y_coord].full){
+                    this.cells[x_coord][y_coord].contents.render(svg)
+                }
+            }
+        }
     }
 }
 
 const grid = new Grid({
-    coords: {x:0,y:0},
+    grid_location: {x:0,y:0},
     cell_size:{
         x_size:1,
         y_size:1
