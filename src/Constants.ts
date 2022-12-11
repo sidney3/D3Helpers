92bb6aff2839b2370a168e2ec7bbef8c173dfa3a
bove1 <90574319+bove1@users.noreply.github.com>
2022-12-10 19:54:48 -0500

    feet: started adding line support (d3 paths)

--- src/Constants.ts
+++ src/Constants.ts
@@ -5,3 +5,5 @@ export const DEFAULT_BORDER_COLOR: string = "rgb(0, 0, 0)"; // Default to black 
 
 export const DEFAULT_FONT_SIZE: number = 15;
 export const DEFAULT_TEXT_COLOR: string = "rgb(0, 0, 0)"
+
+export const DEFAULT_LINE_COLOR: string = "rgb(0, 0, 0)"
